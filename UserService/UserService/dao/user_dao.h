15ceee359e9d0b6b6cbccc70a3c6f5ef8d620f2d
LoveBettygirl <bettycyygirl@163.com>
2022-12-29 09:02:11 +0800

    Update user dao of UserService

--- UserService/UserService/dao/user_dao.h
+++ UserService/UserService/dao/user_dao.h
@@ -4,30 +4,9 @@
 #include <string>
 #include "UserService/lib/mysql/mysql.h"
 #include "UserService/lib/redis/redis.h"
+#include "UserService/dao/user.h"
 
 namespace UserService {
-
-// User表的ORM类
-class User {
-public:
-    User(int id = -1, const std::string &name = "", const std::string &pwd = "", const std::string &state = "offline")
-        : id(id), name(name), password(pwd), state(state) {}
-    
-    void setId(int id) { this->id = id; }
-    void setName(const std::string &name) { this->name = name; }
-    void setPwd(const std::string &pwd) { this->password = pwd; }
-    void setState(const std::string &state) { this->state = state; }
-
-    int getId() const { return id; }
-    std::string getName() const { return name; }
-    std::string getPwd() const { return password; }
-    std::string getState() const { return state; }
-private:
-    int id;
-    std::string name;
-    std::string password;
-    std::string state;
-};
 
 // User表的数据操作类
 class UserDao {
