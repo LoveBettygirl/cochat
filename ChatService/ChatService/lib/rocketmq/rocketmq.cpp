82f9d0c0ecbfd163fc73bc281c848aa64c264479
LoveBettygirl <bettycyygirl@163.com>
2023-01-13 23:03:01 +0800

    Add rocketmq

---
+++ ChatService/ChatService/lib/rocketmq/rocketmq.cpp
@@ -0,0 +1,190 @@
+#include "ChatService/lib/rocketmq/rocketmq.h"
+#include <corpc/common/log.h>
+#include <random>
+#include <algorithm>
+#include <string>
+#include <cctype>
+
+namespace ChatService {
+
+static RocketMQProducerConfig::ptr gRocketMQProducerConfig;
+static int gInitRocketMQProducerConfig = 0;
+
+static RocketMQConsumerConfig::ptr gRocketMQConsumerConfig;
+static int gInitRocketMQConsumerConfig = 0;
+
+RocketMQProducer::RocketMQProducer()
+{
+}
+
+RocketMQProducer::~RocketMQProducer()
+{
+    if (producer_) {
+        producer_->shutdown();
+    }
+}
+
+void RocketMQProducer::loadConf(const YAML::Node &node)
+{
+    if (!gRocketMQProducerConfig) {
+        gRocketMQProducerConfig = std::make_shared<RocketMQProducerConfig>();
+    }
+    gRocketMQProducerConfig->ip = node["ip"].as<std::string>();
+    gRocketMQProducerConfig->port = node["port"].as<int>();
+    gRocketMQProducerConfig->group = node["group"].as<std::string>();
+    gRocketMQProducerConfig->sendMsgTimeout = node["send_msg_timeout"].as<int>();
+    gRocketMQProducerConfig->sendMsgRetries = node["send_msg_retries"].as<int>();
+    gInitRocketMQProducerConfig = 1;
+}
+
+void RocketMQProducer::start()
+{
+    if (!gInitRocketMQProducerConfig) {
+        USER_LOG_ERROR << "RocketMQ Producer config is not initialized!";
+        return;
+    }
+    producer_ = std::make_shared<rocketmq::DefaultMQProducer>(gRocketMQProducerConfig->group);
+    producer_->setNamesrvAddr(gRocketMQProducerConfig->ip + ":" + std::to_string(gRocketMQProducerConfig->port));
+    USER_LOG_INFO << "[RocketMQ] Addr: " << gRocketMQProducerConfig->ip << ":" << gRocketMQProducerConfig->port;
+    producer_->setSendMsgTimeout(gRocketMQProducerConfig->sendMsgTimeout);
+    producer_->start();
+    USER_LOG_INFO << "[RocketMQ] start producer";
+}
+
+std::string RocketMQProducer::randomString(std::string::size_type len)
+{
+    static std::string alphaNumeric("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    std::string result;
+    result.reserve(len);
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::string source(alphaNumeric);
+    std::string::size_type generated = 0;
+    while (generated < len) {
+        std::shuffle(source.begin(), source.end(), generator);
+        std::string::size_type delta = std::min({len - generated, source.length()});
+        result.append(source.substr(0, delta));
+        generated += delta;
+    }
+    return result;
+}
+
+void RocketMQProducer::send(const std::string &topic, const std::string &data)
+{
+    for (int i = 0; i < gRocketMQProducerConfig->sendMsgRetries; i++) {
+        std::string tag = "*", key = randomString(32);
+        try {
+            rocketmq::MQMessage msg(topic, tag, key, data);
+            // 同步生产消息
+            rocketmq::SendResult sendResult = producer_->send(msg);
+            if (sendResult.getSendStatus() == rocketmq::SEND_OK) {
+                USER_LOG_INFO << "[RocketMQ] send msg success: topic: " << topic << " tag: " << tag << " key: " << key << " data: " << data;
+                break;
+            }
+            USER_LOG_ERROR << "[RocketMQ] send msg failed: topic: " << topic << " tag: " << tag << " key: " << key << " data: " << data;
+        }
+        catch (rocketmq::MQException &e) {
+            USER_LOG_ERROR << "[RocketMQ] send msg failed: topic: " << topic << " tag: " << tag << " key: " << key << " data: " << data << " reason: " << e.what();
+        }
+    }
+}
+
+void RocketMQProducer::send(const std::string &topic, const std::string &tag, const std::string &data)
+{
+    for (int i = 0; i < gRocketMQProducerConfig->sendMsgRetries; i++) {
+        std::string key = randomString(32);
+        try {
+            rocketmq::MQMessage msg(topic, tag, key, data);
+            // 同步生产消息
+            rocketmq::SendResult sendResult = producer_->send(msg);
+            if (sendResult.getSendStatus() == rocketmq::SEND_OK) {
+                USER_LOG_INFO << "[RocketMQ] send msg success: topic: " << topic << " tag: " << tag << " key: " << key << " data: " << data;
+                break;
+            }
+            USER_LOG_ERROR << "[RocketMQ] send msg failed: topic: " << topic << " tag: " << tag << " key: " << key << " data: " << data;
+        }
+        catch (rocketmq::MQException &e) {
+            USER_LOG_ERROR << "[RocketMQ] send msg failed: topic: " << topic << " tag: " << tag << " key: " << key << " data: " << data << " reason: " << e.what();
+        }
+    }
+}
+
+void RocketMQProducer::shutdown()
+{
+    producer_->shutdown();
+}
+
+RocketMQConsumer::RocketMQConsumer(RocketMQMsgListener::ptr listener) : listener_(listener)
+{
+}
+
+RocketMQConsumer::~RocketMQConsumer()
+{
+    if (consumer_) {
+        consumer_->shutdown();
+    }
+}
+
+void RocketMQConsumer::loadConf(const YAML::Node &node)
+{
+    if (!gRocketMQConsumerConfig) {
+        gRocketMQConsumerConfig = std::make_shared<RocketMQConsumerConfig>();
+    }
+    gRocketMQConsumerConfig->ip = node["ip"].as<std::string>();
+    gRocketMQConsumerConfig->port = node["port"].as<int>();
+    gRocketMQConsumerConfig->group = node["group"].as<std::string>();
+    gRocketMQConsumerConfig->consumeThreadCount = node["consume_thread_count"].as<int>();
+
+    std::string messageModelStr = node["message_model"].as<std::string>();
+    std::transform(messageModelStr.begin(), messageModelStr.end(), messageModelStr.begin(), tolower);
+    if (messageModelStr == "broadcasting") {
+        gRocketMQConsumerConfig->messageModel = rocketmq::BROADCASTING; // 广播模式，同一个消费者组所有消费者都能消费到同一条消息
+    }
+    else {
+        gRocketMQConsumerConfig->messageModel = rocketmq::CLUSTERING; // 集群模式，同一个消费者组只有一个消费者能消费到同一条消息
+    }
+
+    gInitRocketMQConsumerConfig = 1;
+}
+
+void RocketMQConsumer::subscribe(const std::string &topic)
+{
+    if (!consumer_) {
+        consumer_ = std::make_shared<rocketmq::DefaultMQPushConsumer>(gRocketMQConsumerConfig->group);
+    }
+    consumer_->subscribe(topic, "*");
+}
+
+void RocketMQConsumer::subscribe(const std::string &topic, const std::string &tag)
+{
+    if (!consumer_) {
+        consumer_ = std::make_shared<rocketmq::DefaultMQPushConsumer>(gRocketMQConsumerConfig->group);
+    }
+    consumer_->subscribe(topic, tag);
+}
+
+void RocketMQConsumer::start()
+{
+    if (!gInitRocketMQConsumerConfig) {
+        USER_LOG_ERROR << "RocketMQ Producer config is not initialized!";
+        return;
+    }
+    if (!consumer_) {
+        consumer_ = std::make_shared<rocketmq::DefaultMQPushConsumer>(gRocketMQConsumerConfig->group);
+    }
+    consumer_->setNamesrvAddr(gRocketMQConsumerConfig->ip + ":" + std::to_string(gRocketMQConsumerConfig->port));
+    USER_LOG_INFO << "[RocketMQ] Addr: " << gRocketMQConsumerConfig->ip << ":" << gRocketMQConsumerConfig->port;
+    consumer_->setConsumeThreadCount(gRocketMQConsumerConfig->consumeThreadCount);
+    consumer_->setConsumeFromWhere(rocketmq::CONSUME_FROM_FIRST_OFFSET);
+    consumer_->setMessageModel(rocketmq::BROADCASTING); // 广播模式，同一个消费者组都能消费到消息
+    consumer_->registerMessageListener(listener_.get());
+    consumer_->start();
+    USER_LOG_INFO << "[RocketMQ] start consumer";
+}
+
+void RocketMQConsumer::shutdown()
+{
+    consumer_->shutdown();
+}
+
+}
---
+++ ChatService/ChatService/lib/rocketmq/rocketmq.h
@@ -0,0 +1,110 @@
+#ifndef CHATSERVICE_LIB_ROCKETMQ_ROCKETMQ_H
+#define CHATSERVICE_LIB_ROCKETMQ_ROCKETMQ_H
+
+#include <memory>
+#include <string>
+#include <functional>
+#include <rocketmq/DefaultMQProducer.h>
+#include <rocketmq/DefaultMQPushConsumer.h>
+#include <rocketmq/MQMessage.h>
+#include <yaml-cpp/yaml.h>
+#include <corpc/common/log.h>
+
+namespace ChatService {
+
+class RocketMQProducerConfig {
+public:
+    typedef std::shared_ptr<RocketMQProducerConfig> ptr;
+
+    std::string ip;
+    uint16_t port;
+    std::string group;
+    int sendMsgTimeout;
+    int sendMsgRetries;
+};
+
+class RocketMQConsumerConfig {
+public:
+    typedef std::shared_ptr<RocketMQConsumerConfig> ptr;
+
+    std::string ip;
+    uint16_t port;
+    std::string group;
+    rocketmq::MessageModel messageModel;
+    int consumeThreadCount;
+};
+
+class RocketMQMsgListener : public rocketmq::MessageListenerConcurrently {
+public:
+    typedef std::shared_ptr<RocketMQMsgListener> ptr;
+
+    RocketMQMsgListener(std::function<void(const std::string &, const std::string &)> cb) : callback_(cb) {}
+    virtual ~RocketMQMsgListener() {}
+
+    virtual rocketmq::ConsumeStatus consumeMessage(const std::vector<rocketmq::MQMessageExt> &msgs) {
+        for (int i = 0; i < msgs.size(); i++) {
+            USER_LOG_INFO << "recv msg: topic: " << msgs[i].getTopic() << " key: "  << msgs[i].getKeys() << " tag: " << msgs[i].getTags() << " body: "<<  msgs[i].getBody();
+            callback_(msgs[i].getTags(), msgs[i].getBody());
+        }
+        return rocketmq::CONSUME_SUCCESS;
+    }
+
+private:
+    std::function<void(const std::string &, const std::string &)> callback_;
+};
+
+class RocketMQProducer {
+public:
+    typedef std::shared_ptr<RocketMQProducer> ptr;
+
+    RocketMQProducer();
+    ~RocketMQProducer();
+
+public:
+    // 加载配置信息
+    static void loadConf(const YAML::Node &node);
+
+    void start();
+
+    // 发送消息
+    void send(const std::string &topic, const std::string &data);
+
+    // 发送消息
+    void send(const std::string &topic, const std::string &tag, const std::string &data);
+
+    void shutdown();
+
+private:
+    static std::string randomString(std::string::size_type len);
+
+private:
+    std::shared_ptr<rocketmq::DefaultMQProducer> producer_;
+};
+
+class RocketMQConsumer {
+public:
+    typedef std::shared_ptr<RocketMQConsumer> ptr;
+
+    RocketMQConsumer(RocketMQMsgListener::ptr listener);
+    ~RocketMQConsumer();
+
+public:
+    // 加载配置信息
+    static void loadConf(const YAML::Node &node);
+
+    void subscribe(const std::string &topic);
+
+    void subscribe(const std::string &topic, const std::string &tag);
+
+    void start();
+
+    void shutdown();
+
+private:
+    std::shared_ptr<rocketmq::DefaultMQPushConsumer> consumer_;
+    RocketMQMsgListener::ptr listener_;
+};
+
+}
+
+#endif
